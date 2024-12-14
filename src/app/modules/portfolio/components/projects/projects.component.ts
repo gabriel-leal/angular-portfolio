8fde84a15ebf1d48a67ac0c49ac2fc1920c7e449
gabriel-leal <gabrielealmenezes@gmail.com>
2024-12-14 12:51:27 -0300

    atualizando dados

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -1,8 +1,6 @@
 import { Component, inject, signal } from '@angular/core';
 import {MatDialog, MatDialogModule, MatDialogRef} from '@angular/material/dialog';
-import { EDialogPainelClass } from '../../enum/EDialogPainelClass.enum';
 import { Iprojects } from '../../interface/IProjects.interface';
-import { DialogProjectsComponent } from '../dialog/dialog-projects/dialog-projects.component';
 
 @Component({
   selector: 'app-projects',
@@ -18,65 +16,32 @@ export class ProjectsComponent {
     {
       src: "assets/img/projects/android.png",
       alt: "site sobre o Android",
-      title: "Android",
-      width: "100px",
-      height: "51px",
-      description: "<p>Projeto criado no Curso em Video com HTML5 e CSS3</p>",
-      links: [
-        {
-        name: "Android",
-        site: "https://gabriel-leal.github.io/android/"
-      }
-    ]
+      site: "https://gabriel-leal.github.io/android/"
+
     },
     {
       src: "assets/img/projects/cordelmoderno.png",
       alt: "site sobre o cordel de Milton Duarte",
-      title: "Cordel Moderno",
-      width: "100px",
-      height: "51px",
-      description: "<p>Projeto criado no Curso em Video com HTML5 e CSS3 usando o efeito parallax</p>",
-      links: [
-        {
-        name: "Cordel Moderno",
-        site: "https://gabriel-leal.github.io/cordelmoderno/"
-      }
-    ]
+      site: "https://gabriel-leal.github.io/cordelmoderno/"
     },
     {
       src: "assets/img/projects/inscricaoevento.png",
       alt: "Página sobre a inscrição de um evento",
-      title: "Inscrição Musical",
-      width: "100px",
-      height: "51px",
-      description: "<p>Projeto criado por mim para minha igreja para a inscrição do musical</p>",
-      links: [
-        {
-        name: "Inscrição Musical",
-        site: "https://github.com/gabriel-leal/Inscricao-musical"
-      }
-    ]
+      site: "https://github.com/gabriel-leal/Inscricao-musical"
     },
     {
       src: "assets/img/projects/login.png",
       alt: "projeto de tela de login",
-      title: "Login",
-      width: "100px",
-      height: "51px",
-      description: "<p>Projeto criado no Curso em Video com HTML5 e CSS3</p>",
-      links: [
-        {
-        name: "login",
-        site: "https://gabriel-leal.github.io/login/"
-      }
-    ]
+      site: "https://gabriel-leal.github.io/login/"
+    },
+    {
+      src: "assets/img/projects/lista-de-tarefas.png",
+      alt: "projeto de tela de login",
+      site: "https://github.com/gabriel-leal/angular-lista-de-tarefas"
     },
   ])
 
-  public openDialog(data: Iprojects) {
-    this.#dialog.open(DialogProjectsComponent, {
-      data,
-      panelClass: EDialogPainelClass.PROJECTS
-    })
+  public openSite(data: Iprojects) {
+    window.open(data.site, '_blank');
   }
 }
--- src/app/modules/portfolio/interface/IProjects.interface.ts
+++ src/app/modules/portfolio/interface/IProjects.interface.ts
@@ -1,14 +1,5 @@
 export interface Iprojects {
     src: string
     alt: string
-    title: string
-    width: string
-    height: string
-    description:string
-    links: [
-        {
-            name:string
-            site:string
-        }
-    ]
+    site: string
 }
